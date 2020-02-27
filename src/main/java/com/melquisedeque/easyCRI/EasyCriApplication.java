ac96601ac2c51570f45714683a259f64cb59216d
Melquisedeque Silva <melki2011@gmail.com>
2020-02-27 11:01:00 -0300

    Projeto criado

---
+++ src/main/java/com/melquisedeque/easyCRI/EasyCriApplication.java
@@ -0,0 +1,13 @@
+package com.melquisedeque.easyCRI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCriApplication.class, args);
+	}
+
+}
