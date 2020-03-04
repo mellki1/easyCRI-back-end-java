44603f37b999269561c761041b4db00f971d7f33
Melquisedeque Silva <melki2011@gmail.com>
2020-03-04 16:10:13 -0300

    Adicionando dependencia do banco de dados

--- src/main/java/com/melquisedeque/easyCRI/domain/Cliente.java
+++ src/main/java/com/melquisedeque/easyCRI/domain/Cliente.java
@@ -2,12 +2,21 @@ package com.melquisedeque.easyCRI.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente implements Serializable {
 	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	private String nacionalidade;
 	private String estadoCivil;
