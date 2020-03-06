3dcc25f232b5a6fd90c3d647afb99a309200efc1
Melquisedeque Marcolino da Silva <melki2011@gmail.com>
2020-03-06 20:24:26 -0300

    Update do código para inserir clientes de testes

---
+++ sql insert.sql
@@ -0,0 +1,3 @@
+select * from cliente;
+
+insert into cliente values(1, '13754410601', 'melki2011@gmail.com', 'Avenida Sao Paulo, nº.537, Centro, Santa Vitória-MG','solteiro', 'PE','9413624', 'brasileira', 'Melquisedeque Marcolino da Silva', 'Angela Valentim da Silva', 'Wellington Marcolino da Silva', 'SDS', 'escrevente autorizado', '34998856554');
